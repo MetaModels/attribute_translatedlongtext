247a22649b39c5e2b465402d46cd84125a0e30c2
Mini Model <minimodel@metamodel.me>
2019-03-05 19:43:42 +0100

    Update PHPDoc file headers

--- tests/DependencyInjection/MetaModelsAttributeTranslatedLongtextExtensionTest.php
+++ tests/DependencyInjection/MetaModelsAttributeTranslatedLongtextExtensionTest.php
@@ -1,20 +1,19 @@
 <?php
 
 /**
- * * This file is part of MetaModels/attribute_translatedlongtext.
+ * This file is part of MetaModels/attribute_translatedlongtext.
  *
- * (c) 2012-2017 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage AttributeTranslatedLongtext
+ * @package    MetaModels/attribute_translatedlongtext
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2012-2017 The MetaModels team.
- * @license    https://github.com/MetaModels/attribute_longtext/blob/master/LICENSE LGPL-3.0
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_translatedlongtext/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
